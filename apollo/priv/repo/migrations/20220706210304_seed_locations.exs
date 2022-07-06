74bc687f59cf310b9509ff4373df7489b524fc91
Michael Bower <mikey.bower93@gmail.com>
2022-07-06 22:32:10 +0100

    Missing reference.

--- apollo/priv/repo/migrations/20220706210304_seed_locations.exs
+++ apollo/priv/repo/migrations/20220706210304_seed_locations.exs
@@ -2,8 +2,8 @@ defmodule Apollo.Repo.Migrations.SeedLocations do
   use Ecto.Migration
 
   def change do
-    Travel.create_location(%{name: "Mars"})
-    Travel.create_location(%{name: "Venus"})
-    Travel.create_location(%{name: "Saturn"})
+    Apollo.Travel.create_location(%{name: "Mars"})
+    Apollo.Travel.create_location(%{name: "Venus"})
+    Apollo.Travel.create_location(%{name: "Saturn"})
   end
 end
